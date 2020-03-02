15a05e81a8527ab9a5585ecedfd34d9d21abd403
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-03-02 03:22:57 -0300

    Chore: Seed Admin Created a seed file to generate an admin user;

--- src/database/seeds/20200302060754-admin-user.js
+++ src/database/seeds/20200302060754-admin-user.js
@@ -9,6 +9,7 @@ module.exports = {
           name: 'Distribuidora FastFeet',
           email: 'admin@fastfeet.com',
           password_hash: bcrypt.hashSync('12345678', 8),
+          admin: true,
           created_at: new Date(),
           updated_at: new Date(),
         },
