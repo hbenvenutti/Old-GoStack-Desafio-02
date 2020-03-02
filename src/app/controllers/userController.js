e37b8475be58311f821e34b1c6acad6743414251
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-03-02 03:14:52 -0300

    Feature: Routes & Connection created a route to create users; created a route to list all users; added yup to  validate the requisition when creating a new user;

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,11 +1,34 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async index(req, res) {
-    return res.json({ message: 'hello world' });
+    const users = await User.findAll();
+    return res.json(users);
   }
 
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(8),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Failed' });
+    }
+
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'Email already in use' });
+    }
+
     const { id, name, email, admin } = await User.create(req.body);
 
     return res.json({
