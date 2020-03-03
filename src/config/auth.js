28ec4bf888216b7f9e5d68b3d654a6d14ce69978
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-03-02 22:15:47 -0300

    Feature: Session Authentication created a controller for session; configured jsonwebtoken; created some generic routes and a route to create a session;

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '418802b0536e60bceba30b1f70a46488',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 
-import UserController from './app/controllers/userController';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+routes.put('/users');
+routes.delete('/users');
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
