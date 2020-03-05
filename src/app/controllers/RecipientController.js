31798656441227243409c1e86cd7a20848396405
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-03-05 02:00:27 -0300

    Feature: Recipient Store Configured Yup to validate the data in the store method;

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,3 +1,5 @@
+import * as Yup from 'yup';
+
 import Recipient from '../models/Recipient';
 
 class RecipientController {
@@ -8,8 +10,22 @@ class RecipientController {
   }
 
   async store(req, res) {
-    const { id } = await Recipient.create(req.body);
-    return res.json({ id });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Failed' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+    return res.json(recipient);
   }
 }
 
