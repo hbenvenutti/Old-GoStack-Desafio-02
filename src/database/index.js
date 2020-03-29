6ba14ce4154d53325becf1ef52be8b441ae216f2
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-03-29 02:13:12 -0300

    feature: File uploading -Created a route to upload a image in tmp/uploads folder;

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import databaseConfig from '../config/database';
 
-import User from '../app/models/User';
+import File from '../app/models/File';
 import Recipient from '../app/models/Recipient';
+import User from '../app/models/User';
 
-const models = [User, Recipient];
+const models = [User, Recipient, File];
 
 class Database {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import RecipientController from './app/controllers/RecipientController';
+import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 import authAdmin from './app/middlewares/authAdmin';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
@@ -17,6 +22,7 @@ routes.get('/recipients', RecipientController.index);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.use(authAdmin);
 
